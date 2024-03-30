dfe0e767f5d20802b8a3f3cd2177c67066cf1a43
itzaRinna <dutchismahdick@gmail.com>
2024-03-30 20:10:01 +0700

    new files coming ambatukam

---
+++ Godot_v4.2.1-stable_mono_linux_x86_64/RunGodot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Change directory to where your Godot project is located
+cd /home/itzarinna/Desktop/Godot_v4.2.1-stable_mono_linux_x86_64
+
+# Run Godot with the specified parameters
+'/home/itzarinna/Desktop/Godot_v4.2.1-stable_mono_linux_x86_64/Godot_v4.2.1-stable_mono_linux.x86_64' --render-driver opengl3
+
+
